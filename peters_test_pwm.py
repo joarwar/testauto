0a4aab4c10a95f6c4fd0ad0964285ce137e45092
peter <petlei0802@skola.goteborg.se>
2024-10-09 08:51:04 +0200

    Test code and a LTspice-file
    Skiss över class D-förstärkarens princip och lite gammal testkod för att göra en PWM.

---
+++ peters_test_pwm.py
@@ -0,0 +1,15 @@
+from gpiozero import LED
+from time import sleep
+
+my_led = LED(3)
+off_time = 1
+on_time = 0
+
+for i in range(5):
+    for j in range(1,1001):
+        my_led.on()
+        sleep(on_time)
+        on_time += 0.001
+        my_led.off()
+        sleep(off_time)
+        off_time += 0.001
