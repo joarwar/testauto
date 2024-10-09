019b44ea0444f4bfcbe0a16f3807d288d7e1d315
peter <petlei0802@skola.goteborg.se>
2024-10-09 13:59:01 +0200

    Made a txt file with super secret passwords

--- peters_test_pwm.py
+++ peters_test_pwm.py
@@ -9,7 +9,7 @@ for i in range(5):
     for j in range(1,1001):
         my_led.on()
         sleep(on_time)
-        on_time += 0.001
+        on_time += 0.0010
         my_led.off()
         sleep(off_time)
         off_time += 0.001
