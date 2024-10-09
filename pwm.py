5a635319d86ea2802b305449dd133fdb1d59f1ee
Joar Warholm <joawar0713@skola.goteborg.se>
2024-10-09 15:39:03 +0200

    added time on pwm

--- pwm.py
+++ pwm.py
@@ -10,7 +10,7 @@ pi_pwm = GPIO.PWM(ledpin, 2000)  # Create PWM instance with frequency
 pi_pwm.start(0)  # Start PWM with required Duty Cycle
 
 try:
-    for x in range(1000):
+    for x in range(10000):
         for angle in range(0, 360, 5):  # Loop through angles from 0 to 360 degrees in 5-degree increments
             # Calculate the sine value and convert it to duty cycle (0-100%)
             duty = (math.sin(math.radians(angle)) + 1) * 50  # Scale to 0-100%
